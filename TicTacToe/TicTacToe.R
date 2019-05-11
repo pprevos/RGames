21c7326c7a8feb0818413707de356e589cd64fd1
pprevos <peter@prevos.net>
2019-05-11 17:08:18 +1000

    Updated Tic Tac Toe

--- TicTacToe/TicTacToe.R
+++ TicTacToe/TicTacToe.R
@@ -1,8 +1,8 @@
-# TIC TAC TOE
-# The Devil is in the Data
-# peter@prevos.net
+## Tic Tac Toe Simulation in the R language
+## Peter Prevos
+## lucidmanager.org
 
-# Draw the game board
+## Draw the game board
 draw.board <- function(game) {
     xo <- c("X", " ", "O") # Symbols
     par(mar = rep(1,4))
@@ -12,7 +12,7 @@ draw.board <- function(game) {
     abline(v = c(10, 20), col = "darkgrey", lwd = 4)
     text(rep(c(5, 15, 25), 3), c(rep(25, 3), rep(15,3), rep(5, 3)), xo[game + 2], cex = 4)
     # Identify location of any three in a row
-    square <- t(matrix(game, nrow = 3))
+    square <- matrix(game, nrow = 3, byrow = TRUE)
     hor <- abs(rowSums(square))
     if (any(hor == 3)) 
         hor <- (4 - which(hor == 3)) * 10 - 5 
@@ -26,13 +26,19 @@ draw.board <- function(game) {
     diag1 <- sum(diag(square))
     diag2 <- sum(diag(t(apply(square, 2, rev)))) 
     # Draw winning lines 
-    if (all(hor > 0)) for (i in hor) lines(c(0, 30), rep(i, 2), lwd = 10, col="red")
-    if (all(ver > 0)) for (i in ver) lines(rep(i, 2), c(0, 30), lwd = 10, col="red")
-    if (abs(diag1) == 3) lines(c(2, 28), c(28, 2), lwd = 10, col = "red")
-    if (abs(diag2) == 3) lines(c(2, 28), c(2, 28), lwd = 10, col = "red")
+    if (all(hor > 0))
+        for (i in hor)
+            lines(c(0, 30), rep(i, 2), lwd = 10, col="red")
+    if (all(ver > 0))
+        for (i in ver)
+            lines(rep(i, 2), c(0, 30), lwd = 10, col="red")
+    if (abs(diag1) == 3)
+        lines(c(2, 28), c(28, 2), lwd = 10, col = "red")
+    if (abs(diag2) == 3)
+        lines(c(2, 28), c(2, 28), lwd = 10, col = "red")
 }
 
-# Human player enters a move
+## Human player enters a move
 move.human <- function(game) {
     text(4, 0, "Click on screen to move", col = "grey", cex=.7)
     empty <- which(game == 0)
@@ -43,75 +49,72 @@ move.human <- function(game) {
         coords$y <- floor(abs(coords$y) / 10) + 1
         move <- coords$x + 3 * (3 - coords$y)
     }
-    return (move)
-}
-
-# Evaluate board position
-eval.game <- function(game, player) {
-    # Determine game score
-    square <- t(matrix(game, nrow = 3))
-    hor <- rowSums(square)
-    ver <- colSums(square)
-    diag1 <- sum(diag(square))
-    diag2 <- sum(diag(t(apply(square, 2, rev))))
-    eval <- c(hor, ver, diag1, diag2)
-    # Determine best score
-    minimax <- ifelse(player == -1, "min", "max")
-    best.score <- do.call(minimax, list(eval))
-    if (abs(best.score) == 3) best.score <- best.score * 2
-    return (best.score)
-}
-
-# Determine computer move
-move.computer <- function(game, player) {
-    empty <- which(game == 0)
-    eval <- matrix(nrow = 10, ncol = 9, data = 0)
-    for (i in empty) {
-        game.tmp <- game
-        game.tmp[i] <- player
-        eval[1, i] <- eval.game(game.tmp, player)
-        empty.tmp <- which(game.tmp ==0)
-        for (j in empty.tmp) {
-            game.tmp1 <- game.tmp
-            game.tmp1[j] <- -player
-            eval[(j + 1), i] <- eval.game(game.tmp1, -player)
-        }
-    }
-    if (!any(abs(eval[1,]) == 6)) { # When winning, play move
-        # Analyse opponent move
-        minimax <- ifelse(player == -1, "max", "min") # Minimax
-        best.opponent <- apply(eval[-1,], 1, minimax)
-        eval[1,] <- eval[1,] * -player * best.opponent
-    }
-    # Add randomisation and strategic values
-    board <- c(3, 2, 3, 2, 4, 2, 3, 2, 3) # Strategic values
-    board <- sapply(board, function(x) runif(1, 0.1 * x, (0.1 * x) + 0.1)) # Randomise
-    eval[1, empty] <- eval[1, empty] + player * board[empty] # Randomise moves
-    # Pick best game
-    minimax <- ifelse(player == -1, "which.min", "which.max") # Minimax
-    move <- do.call(minimax, list(eval[1,])) # Select best move
     return(move)
 }
 
-# Main game engine
+## Minimax code by Alberto C.
+ganador <- function(juego, player) {
+    game <- matrix(juego, nrow = 3, byrow = T)
+    hor <- rowSums(game)
+    ver <- colSums(game)
+    diag <- c(sum(diag(game)), sum(diag(apply(game, 1, rev))))
+    if (-3 %in% c(hor, ver, diag))
+        return(-10)
+    if (3 %in% c(hor, ver, diag))
+        return(10)
+    else
+        return(0)
+}
+
+minimax <- function(juego, player) {
+    free <- which(juego == 0)
+    if(length(free) == 1) {
+        juego[free] <- player
+        return(list(move = free, U = ganador(juego, player)))
+    }
+    poss.results <- rep(0, 9)
+    for(i in free) {
+        game <- juego
+        game[i] <- player
+        poss.results[i] <- ganador(game, player)
+    }
+    mm <- ifelse(player == -1, "which.min", "which.max")
+    if(any(poss.results == (player * 10))) {
+        move <- do.call(mm, list(poss.results))
+        return(list(move = move, U = poss.results[move]))
+    }
+    for(i in free) {
+        game <- juego
+        game[i] <- player
+        poss.results[i] <- minimax(game, -player)$U
+    }
+    random <- runif(9, 0, 0.1)
+    poss.results[-free] <- 100 * -player
+    poss.results <- poss.results + (player * random)
+    move <- do.call(mm, list(poss.results))
+    return(list(move = move, U = poss.results[move]))
+}
+
+## Main game engine
 tic.tac.toe <- function(player1 = "human", player2 = "computer") {
     game <- rep(0, 9) # Empty board
-    winner <- FALSE # Define winner
+    winner <- 0 # Define winner
     player <- 1 # First player
     players <- c(player1, player2)
     draw.board(game)
-    while (0 %in% game & !winner) { # Keep playing until win or full board
+    while (0 %in% game & winner == 0) { # Keep playing until win or full board
         if (players[(player + 3) %% 3] == "human") # Human player
             move <- move.human(game)
-        else # Computer player
-            move <- move.computer(game, player)
+        else { # Computer player
+            move <- minimax(game, player)[[1]]
+            }
         game[move] <- player # Change board
         draw.board(game)
-        winner <- max(eval.game(game, 1), abs(eval.game(game, -1))) == 6 # Winner, winner, chicken dinner?
+        winner <- ganador(game, player)
         player <- -player # Change player
     }
+    if (winner == 0)
+        text(15, 15 , "DRAW", col = "red", cex = 10)
 }
 
 tic.tac.toe()
-
-
--- TicTacToe/WarGames.R
+++ TicTacToe/WarGames.R
@@ -1,7 +1,8 @@
-# WAR GAMES TIC TAC TOE
-source("Tic Tac Toe/Tic Tac Toe.R")
+## Load Tic Tac Toe functions
+source("TicTacToe.R")
+library(animation)
 
-# Draw the game board
+## WAR GAMES TIC TAC TOE
 draw.board.wargames <- function(game) {
     xo <- c("X", " ", "O") # Symbols
     par(mar = rep(1,4), bg = "#050811")
@@ -9,9 +10,10 @@ draw.board.wargames <- function(game) {
     plot.window(xlim = c(0,30), ylim = c(0,30))
     abline(h = c(10, 20), col = "#588fca", lwd = 20)
     abline(v = c(10, 20), col = "#588fca", lwd = 20)
-    text(rep(c(5, 15, 25), 3), c(rep(25, 3), rep(15,3), rep(5, 3)), xo[game + 2], cex = 20, col = "#588fca")
-    text(1,0,"r.prevos.net", col = "#588fca", cex=2)
-    # Identify location of any three in a row
+    text(rep(c(5, 15, 25), 3), c(rep(25, 3), rep(15,3), rep(5, 3)), xo[game + 2],
+         cex = 20, col = "#588fca")
+    text(0, 0, "lucidmanager.org", col = "#588fca", cex = 2, adj = 0)
+    ## Identify location of any three in a row
     square <- t(matrix(game, nrow = 3))
     hor <- abs(rowSums(square))
     if (any(hor == 3)) 
@@ -25,32 +27,33 @@ draw.board.wargames <- function(game) {
         ver <- 0
     diag1 <- sum(diag(square))
     diag2 <- sum(diag(t(apply(square, 2, rev)))) 
-    # Draw winning lines 
-    if (all(hor > 0)) for (i in hor) lines(c(0, 30), rep(i, 2), lwd = 10, col="#588fca")
-    if (all(ver > 0)) for (i in ver) lines(rep(i, 2), c(0, 30), lwd = 10, col="#588fca")
-    if (abs(diag1) == 3) lines(c(2, 28), c(28, 2), lwd = 10, col = "#588fca")
-    if (abs(diag2) == 3) lines(c(2, 28), c(2, 28), lwd = 10, col = "#588fca")
+    ## Draw winning lines 
+    if (all(hor > 0)) for (i in hor) lines(c(0, 30), rep(i, 2), lwd = 20, col="#588fca")
+    if (all(ver > 0)) for (i in ver) lines(rep(i, 2), c(0, 30), lwd = 20, col="#588fca")
+    if (abs(diag1) == 3) lines(c(2, 28), c(28, 2), lwd = 20, col = "#588fca")
+    if (abs(diag2) == 3) lines(c(2, 28), c(2, 28), lwd = 20, col = "#588fca")
 }
 
-library(animation)
-player <- -1
-games <- 100
 saveGIF ({
-    for (i in 1:games) {
+    for (i in 1:10) {
         game <- rep(0, 9) # Empty board
         winner <- 0 # Define winner
-        #draw.board.wargames(game)
-        while (0 %in% game & !winner) { # Keep playing until win or full board
-            empty <- which(game == 0)
-            move <- move.computer(game, player)
-            game[move] <- player
-            if (i <= 12) draw.board.wargames(game)
-            winner <- max(eval.game(game, 1), abs(eval.game(game, -1))) == 6
-            player <- -player
+        player <- -1 # First 
+        while (0 %in% game & winner == 0) { # Keep playing until win or full board
+            empty <- which(game == 0) # Define empty squares
+            if (length(empty) == 9)
+                move <- empty[sample(length(empty), 1)] # Random move
+            else
+                move <- minimax(game, player)[[1]]
+            game[move] <- player # Change board
+            draw.board.wargames(game)
+            winner <- ganador(game) # Evaulate game
+            player <- -player # Change player
         }
-        if (i > 12) draw.board.wargames(game)
     }
 },
-interval = c(unlist(lapply(seq(1, 0,-.2), function (x) rep(x, 9))), rep(0,9*94)), 
-movie.name = "wargames.gif", ani.width = 1024, ani.height = 1024)
+interval = .1, movie.name = "wargames.gif",
+ani.width = 1024, ani.height = 1024)
+
+
 
