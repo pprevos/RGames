f538cf0c35f3162348154dc6f6d64445755c8e2f
Peter Prevos <peter@prevos.net>
2022-02-21 10:37:19 +1100

    Create wordle_cheat.R
    New file

---
+++ wordle_cheat.R
@@ -0,0 +1,16 @@
+# Extract current Wordle solution
+
+library(stringr)
+
+js <- readLines("https://www.nytimes.com/games/wordle/main.bd4cb59c.js")[1]
+
+solutions <- str_extract(js[1], 'Ma=\\[[a-z|\\"|\\,]+') %>% 
+  strsplit(",") %>% 
+  unlist() %>% 
+  str_remove_all('Ma=\\[|[:punct:]')
+
+# New solutions at 13:00 UTC
+current_time <- as.POSIXct(format(Sys.time(), tz = "UTC"))
+start_time <- as.POSIXct("2021-06-17 13:00:00", tz = "UTC")
+
+solutions[ceiling(as.numeric(current_time - start_time))]
